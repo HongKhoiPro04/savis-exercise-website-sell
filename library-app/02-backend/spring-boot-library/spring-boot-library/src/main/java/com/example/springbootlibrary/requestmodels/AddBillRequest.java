7879b66ff0f3fa8c3802077beb12e5b1800c733c
cuongdanglamgiau <cuongbcph27577@fpt.edu.vn>
2023-11-20 13:05:16 +0700

    update table(bill+size)

---
+++ library-app/02-backend/spring-boot-library/spring-boot-library/src/main/java/com/example/springbootlibrary/requestmodels/AddBillRequest.java
@@ -0,0 +1,47 @@
+package com.example.asm_be.request;
+
+import com.example.asm_be.entities.Bill;
+import com.example.asm_be.entities.Users;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AddBillRequest {
+    private String userName;
+    private String phone;
+    private Integer idBill;
+    private Integer provinceId;
+    private Integer districtId;
+    private String wardId;
+    private String address;
+    private String note;
+    private Double fee;
+    private int optionPay;
+    private Double totalPay;
+    private Date shipDate;
+
+    public Bill map(Bill bill, Users users){
+        bill.setCreatedAt(new Date());
+        bill.setAddress(this.address);
+        bill.setDistrict(this.districtId);
+        bill.setProvince(this.provinceId);
+        bill.setWard(this.wardId);
+        bill.setFee(this.fee);
+        bill.setPaymentOptions(this.optionPay);
+        bill.setTotalPay(this.totalPay);
+        bill.setDescription(this.note);
+        bill.setShipDate(this.shipDate);
+        bill.setUsers(users);
+        return bill;
+    }
+
+
+}
---
+++ library-app/02-backend/spring-boot-library/spring-boot-library/src/main/java/com/example/springbootlibrary/service/Impl/BillDetailImpl.java
@@ -0,0 +1,86 @@
+package com.example.asm_be.service.impl;
+
+import com.example.asm_be.entities.*;
+import com.example.asm_be.repositories.*;
+import com.example.asm_be.request.BillDetailsRequest;
+import com.example.asm_be.service.AddressService;
+import com.example.asm_be.service.BillDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class BillDetailImpl implements BillDetailService {
+    @Autowired
+    private BillDetailsRepository billDetailsRepository;
+    @Autowired
+    private CartDetailsRepository cartDetailsRepository;
+    @Autowired
+    private ProductDetailRepository productDetailRepository;
+    @Autowired
+    private BillRepository billRepository;
+
+    @Override
+    public List<BillDetails> getAll(int idBill) {
+        return billDetailsRepository.findByBillId(idBill);
+    }
+
+    @Override
+    public BillDetails getOne(int id) {
+        return billDetailsRepository.findById(id).get();
+    }
+
+    @Override
+    public List<BillDetails> save( int idBill, int idCart) {
+        List<CartDetails> listCartDt = cartDetailsRepository.findByCartId(idCart);
+        Bill bill = billRepository.findById(idBill).orElse(null);
+        List<BillDetails> billDetailsList = new ArrayList<>();
+        if (bill != null) {
+            for (CartDetails cartDetails : listCartDt) {
+                BillDetails billDetail = new BillDetails();
+                billDetail.setBill(bill);
+                billDetail.setProductDetail(cartDetails.getProductDetail());
+                billDetail.setQuantity(cartDetails.getQuantity());
+                Optional<ProductDetail> productDetail = productDetailRepository.findById(cartDetails.getProductDetail().getId());
+                ProductDetail prDtOut = productDetail.get();
+                prDtOut.setQuantity(prDtOut.getQuantity() - cartDetails.getQuantity());
+                billDetail.setPrice(cartDetails.getProductDetail().getPrice());
+                billDetailsRepository.save(billDetail);
+                billDetailsList.add(billDetail);
+            }
+        }
+        return billDetailsList;
+    }
+
+    @Override
+    public Double getTongGia(List<BillDetailsRequest> list) {
+        Double result = 0.0;
+
+        for (int i = 0; i < list.size(); ++i) {
+            Double giaBan = 0.0;
+            ProductDetail ctsp = this.productDetailRepository.findById((list.get(i)).getPrDetailId()).get();
+            if (ctsp.getPromotional() != null) {
+                giaBan = ctsp.getPrice() - ctsp.getPrice() * ctsp.getPromotional().getValue() / 100.0;
+            } else {
+                giaBan = ctsp.getPrice();
+            }
+            result = result + (double) (list.get(i)).getQuantity() * giaBan;
+        }
+        return result;
+    }
+
+
+    @Override
+    public BillDetails update(BillDetails billDetail) {
+        return billDetailsRepository.save(billDetail);
+    }
+
+    @Override
+    public void delete(BillDetails billDetail) {
+        billDetailsRepository.delete(billDetail);
+    }
+}
